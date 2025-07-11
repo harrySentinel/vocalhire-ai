8c1c6c59c248f4a589255169e00d499b0e62a8ad
harrySentinel <adityasrivastav9721057380@gmail.com>
2025-07-11 19:27:37 +0530

    created form field as a separate reusable component

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -25,49 +25,36 @@ const formSchema = z.object({
 const AuthForm = () => {
 
   // 1. Define your form.
- const form = useForm<z.infer<typeof formSchema>>({
-   resolver: zodResolver(formSchema),
-   defaultValues: {
-    username: "",
-   },
- })
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+    },
+  })
 
- // 2. Define a submit handler
-   function onSubmit(values: z.infer<typeof formSchema>) {
+  // 2. Define a submit handler
+  function onSubmit(values: z.infer<typeof formSchema>) {
     console.log(values)
-   }
+  }
 
   return (
     <div className="card-border lg:min-w-[566px]">
       <div className="flex flex-col gap-6 card py-14 px-10">
         <div className="flex flex-row gap-2 justify-center">
-            <Image src='/logo.svg' alt="logo" height={32} width={38} />
+          <Image src='/logo.svg' alt="logo" height={32} width={38} />
 
-            <h2 className="text-primary-100">Vocalhire AI</h2>
+          <h2 className="text-primary-100">Vocalhire AI</h2>
         </div>
 
+        <h3>Practice job interview with AI</h3>
+
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="w-full space-y-6 mt-4 form">
+           
+            <Button type="submit">Submit</Button>
+          </form>
+        </Form>
       </div>
-      <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        <FormField
-          control={form.control}
-          name="username"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Username</FormLabel>
-              <FormControl>
-                <Input placeholder="shadcn" {...field} />
-              </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button type="submit">Submit</Button>
-      </form>
-    </Form>
     </div>
   )
 }
