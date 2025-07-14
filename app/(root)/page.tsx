705000c80a4440ac447ccc90fc38e1ebea258633
harrySentinel <adityasrivastav9721057380@gmail.com>
2025-07-14 23:29:45 +0530

    started working on landing page - hero section

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,25 @@
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
 import React from 'react'
 
 const Page = () => {
   return (
-    <div>Home Page</div>
+     <>
+       <section className='card-cta'>
+         <div className='flex flex-col gap-6 max-w-lg'>
+            <h2>Get Interview-Ready with AI-Powered Practice & Feedback</h2>
+
+            <p className='text-lg'>
+              Practice on real interview questions & get instant feedback
+            </p>
+
+            <Button asChild className='btn-primary max-sm:w-full'>
+
+              <Link href='/interview'>Start an Interview</Link>
+            </Button>
+         </div>
+       </section>
+     </>
   )
 }
 
