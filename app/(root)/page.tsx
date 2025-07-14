0c5314f7c8a4477edb671b8e4d2ae0ad9e2d90aa
harrySentinel <adityasrivastav9721057380@gmail.com>
2025-07-14 23:59:44 +0530

    fix issue

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,5 @@
 import { Button } from '@/components/ui/button'
+import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
@@ -18,6 +19,24 @@ const Page = () => {
               <Link href='/interview'>Start an Interview</Link>
             </Button>
          </div>
+
+          <Image src="/robot.png" alt="robo-dude" width={400} height={400} className="max-sm:hidden" />
+       </section>
+
+       <section className='flex flex-col gap-6 mt-8'>
+            <h2>Your Interviews</h2>
+
+            <div className='interviews-section'>
+               <p>You haven&apos;t taken any interviews yet</p>
+            </div>
+       </section>
+
+       <section className='flex flex-col gap-6 mt-8'>
+             <h2>Take an Interview</h2>
+
+           <div className='interviews-section'>
+             <p>There are no interviews available</p>
+           </div>
        </section>
      </>
   )
