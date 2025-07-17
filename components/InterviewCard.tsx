4c38187f04a0b4f6fd3bc3521887311727588a97
harrySentinel <adityasrivastav9721057380@gmail.com>
2025-07-18 00:17:20 +0530

    added types folder for comprehensive type system

--- components/InterviewCard.tsx
+++ components/InterviewCard.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
-const InterviewCard = () => {
+const InterviewCard = ({interviewId, userId, role, type, techstack, createdAt}: InterviewCardProps) => {
+    const feedback = null as Feedback | null;
   return (
     <div>InterviewCard</div>
   )
