fe7b512b1efb49bf486b9264bbdeee6774b37538
harrySentinel <adityasrivastav9721057380@gmail.com>
2025-07-19 23:59:33 +0530

    added key in interview section

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -40,7 +40,7 @@ const Page = () => {
 
            <div className='interviews-section'>
              {dummyInterviews.map((interview) => (
-                <InterviewCard {...interview}/>
+                <InterviewCard {...interview} key={interview.id}/>
                ))}
 
               {/*<p>You haven&apos;t taken any interviews</p>*/}
