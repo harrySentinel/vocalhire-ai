9dab457fa52b5e618803c3371fbc0bd589abf685
harrySentinel <adityasrivastav9721057380@gmail.com>
2025-07-19 23:57:30 +0530

    added date format

--- components/InterviewCard.tsx
+++ components/InterviewCard.tsx
@@ -3,7 +3,7 @@ import dayjs from 'dayjs';
 const InterviewCard = ({interviewId, userId, role, type, techstack, createdAt}: InterviewCardProps) => {
     const feedback = null as Feedback | null;
     const normalizedType = /mix/gi.test(type) ? 'Mixed' : type;
-    const formattedDate = dayjs(feedback?.createdAt || createdAt || Date.now())
+    const formattedDate = dayjs(feedback?.createdAt || createdAt || Date.now()).format('MMM D, YYYY');
   return (
     <div>InterviewCard</div>
   )
